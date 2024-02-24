0b3cc4f204df5c3b0c2a46ed2c3806bd555f829f
Jovinull <felipejovinogamerplay@gmail.com>
2024-02-24 19:01:25 -0300

    Coments and Readme Update

--- material/aulas/test_cob_ambiente.cob
+++ material/aulas/test_cob_ambiente.cob
@@ -1,11 +1,10 @@
-* Programa de Teste para Verificar o Funcionamento do COBOL
-
        IDENTIFICATION DIVISION.
        PROGRAM-ID. TESTCOB.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        77 WS-NOME  PIC X(10).
        PROCEDURE DIVISION.
+      * Codigo para testar o funcionamento do ambiente COBOL
            DISPLAY "DIGITE O SEU NOME: "
            ACCEPT WS-NOME
            DISPLAY "O NOME DIGITADO E: " WS-NOME
